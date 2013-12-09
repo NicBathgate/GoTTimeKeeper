1613d2cb3b253317860a2178102507baeb4a379e
Nic Bathate <balinornz@gmail.com>
2013-12-09 19:36:23 +1300

    commit before format

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 	// initialize houses page to be hidden
 	$(".houses").hide();
 
-	$(".gamelink").bind('touchstart', toggle_tabs);
-	$(".houselink").bind('touchstart', toggle_tabs);
+	$(".gamelink").bind('touchstart click', toggle_tabs);
+	$(".houselink").bind('touchstart click', toggle_tabs);
 
 	var elapsed = [];
 	interval_ids = new Array();
 
-	$(".btn").bind('touchstart', function(event) {
+	$(".btn").bind('touchstart click', function(event) {
 		event.stopImmediatePropagation(); event.preventDefault();
 		var button_text = $(this).html().split('<br', 1);
 		var house = $.trim(button_text).toLowerCase();
